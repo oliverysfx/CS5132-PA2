da87801bf6a0b52a6ffd5fcff8e21dfa84a4c20c
oliverysfx <oliverysfx@outlook.com>
2022-10-09 18:58:08 +0800

    Use a different definition of Gini Index

--- Metrics.java
+++ Metrics.java
@@ -3,32 +3,24 @@ import java.util.ArrayList;
 public class Metrics{
     /**
      * Calculates the Gini Index of a decision node
-     * @param groups an array of ArrayLists split from decision node containing data
+     * @param group an array list containing data
      * @param classes the classes that the classifier should produce
      * @return The Gini Index
      */
-    public static double gini(ArrayList<double[]>[] groups, double[] classes){
-        double totalSize = 0;
-        for (ArrayList<double[]> group : groups) {
-            totalSize += group.size();
+    public static double gini(ArrayList<double[]> group, double[] classes){
+        double size = group.size();
+        if(size == 0){
+            return 0;
         }
-        double result = 0;
-        for (ArrayList<double[]> group : groups){
-            double size = group.size();
-            if(size == 0){
-                continue;
+        double score = 0;
+        for (double class_val : classes){
+            double p = 0;
+            for(double[] data : group){
+                p += data[data.length-1] == class_val ? 1 : 0;
             }
-            double score = 0;
-            for (double class_val : classes){
-                double p = 0;
-                for(double[] data : group){
-                    p += data[data.length-1] == class_val ? 1 : 0;
-                }
-                p /= size;
-                score += p * p;
-            }
-            result += (1.0 - score) * size / totalSize;
+            p /= size;
+            score += p * p;
         }
-        return result;
+        return score;
     }
 }
